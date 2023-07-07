b3b63c2e74e9f87c3e42eefc675a40d021dae73b
akinadejoro-nc <akinyemi.adejoro@nordcloud.com>
2023-07-07 12:22:42 +0100

    Trivy scan added to the Vulnerability scan stage with bash testing

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -5,3 +5,16 @@ echo $dockerImageName
 
 docker run --rm -v /tmp/.cache:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity HIGH --light $dockerImageName
 docker run --rm -v /tmp/.cache:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity CRITICAL --light $dockerImageName
+
+    # Trivy Scan result processing
+    exit_code=$?
+    echo "Exit Code : $exit_code"
+
+    # Check scan results
+    if [ "$(exit_code)" == 1 ]
+    then
+        echo "Image scanning failed. CRITICAL Vulnerability found"
+        exit 1;
+    else
+        echo "Image scanning passed. No CRITICAL Vulnerability found"
+    fi
