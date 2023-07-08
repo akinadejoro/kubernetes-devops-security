6968bd717e99d46c365a172a7d942b4da80187e9
akinadejoro-nc <akinyemi.adejoro@nordcloud.com>
2023-07-08 09:34:34 +0100

    Kubesec scan added

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -6,7 +6,7 @@ sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
 kubectl -n default get deployment ${deploymentName} > /dev/null
 
 if [[ $? -ne 0 ]]; then
-    echo "deployment ${deploymentName} doesnt exist"
+    echo "deployment ${deploymentName} doesn't exist"
     kubectl -n default apply -f k8s_deployment_service.yaml
 else
     echo "deployment ${deploymentName} exist"
