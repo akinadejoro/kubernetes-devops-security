ff4750351638fa639a7b88d5bd9b2b0dc25bbac7
akinadejoro-nc <akinyemi.adejoro@nordcloud.com>
2023-07-11 17:37:18 +0100

    test k8s docker image scan test with export

--- trivy-k8s-scan.sh
+++ trivy-k8s-scan.sh
@@ -13,8 +13,8 @@ echo $USER_CREDENTIALS_PSW
 # TRIVY_USERNAME=$USER_CREDENTIALS_USR TRIVY_PASSWORD=$USER_CREDENTIALS_PSW docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
 # TRIVY_USERNAME=$USER_CREDENTIALS_USR TRIVY_PASSWORD=$USER_CREDENTIALS_PSW docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $imageName
 
-docker run --rm -v /tmp/.cache:/root/.cache/ aquasec/trivy:0.17.2 TRIVY_USERNAME=$USER_CREDENTIALS_USR TRIVY_PASSWORD=$USER_CREDENTIALS_PSW -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light --input $imageName
-docker run --rm -v /tmp/.cache:/root/.cache/ aquasec/trivy:0.17.2 TRIVY_USERNAME=$USER_CREDENTIALS_USR TRIVY_PASSWORD=$USER_CREDENTIALS_PSW -q image --exit-code 0 --severity CRITICAL --light --input $imageName
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e "TRIVY_USERNAME=$USER_CREDENTIALS_USR" -e "TRIVY_PASSWORD=$USER_CREDENTIALS_PSW" aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light --input $imageName
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e "TRIVY_USERNAME=$USER_CREDENTIALS_USR" -e "TRIVY_PASSWORD=$USER_CREDENTIALS_PSW" aquasec/trivy:0.17.2 -q image --exit-code 0 --severity CRITICAL --light --input $imageName
 
     # Trivy scan result processing
     exit_code=$?
