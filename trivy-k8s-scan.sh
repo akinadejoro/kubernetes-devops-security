cee682ef3d9dd31672b222383dbf12aeb3ffba16
akinadejoro-nc <akinyemi.adejoro@nordcloud.com>
2023-07-11 17:08:13 +0100

    test k8s docker image scan test with export

--- trivy-k8s-scan.sh
+++ trivy-k8s-scan.sh
@@ -8,18 +8,14 @@ wk_dir=`pwd`
 
 echo $wk_dir
 
-
-
-git clone $imageName
-
 # export $USER_CREDENTIALS_USR
 # export $USER_CREDENTIALS_PSW
 
 # TRIVY_USERNAME=$USER_CREDENTIALS_USR TRIVY_PASSWORD=$USER_CREDENTIALS_PSW docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
 # TRIVY_USERNAME=$USER_CREDENTIALS_USR TRIVY_PASSWORD=$USER_CREDENTIALS_PSW docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $imageName
 
-docker run --rm -v /tmp/.cache:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light --input "$WORKSPACE/$imageName"
-docker run --rm -v /tmp/.cache:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity CRITICAL --light --input "$WORKSPACE/$imageName"
+docker run --rm -v /tmp/.cache:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light --input $imageName
+docker run --rm -v /tmp/.cache:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity CRITICAL --light --input $imageName
 
     # Trivy scan result processing
     exit_code=$?
