98154f4072de782985c09ac1ca04b465f4705a38
akinadejoro-nc <akinyemi.adejoro@nordcloud.com>
2023-07-11 17:45:31 +0100

    test k8s docker image scan test with export

--- trivy-k8s-scan.sh
+++ trivy-k8s-scan.sh
@@ -4,17 +4,19 @@
 
 echo $imageName #getting Image name from env variable
 
-export $USER_CREDENTIALS_USR
-export $USER_CREDENTIALS_PSW
+# export $USER_CREDENTIALS_USR
+# export $USER_CREDENTIALS_PSW
 
-echo $USER_CREDENTIALS_USR
-echo $USER_CREDENTIALS_PSW
+# echo $USER_CREDENTIALS_USR
+# echo $USER_CREDENTIALS_PSW
 
-# TRIVY_USERNAME=$USER_CREDENTIALS_USR TRIVY_PASSWORD=$USER_CREDENTIALS_PSW docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
-# TRIVY_USERNAME=$USER_CREDENTIALS_USR TRIVY_PASSWORD=$USER_CREDENTIALS_PSW docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $imageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $imageName
 
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e "TRIVY_USERNAME=$USER_CREDENTIALS_USR" -e "TRIVY_PASSWORD=$USER_CREDENTIALS_PSW" aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light --input $imageName
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e "TRIVY_USERNAME=$USER_CREDENTIALS_USR" -e "TRIVY_PASSWORD=$USER_CREDENTIALS_PSW" aquasec/trivy:0.17.2 -q image --exit-code 0 --severity CRITICAL --light --input $imageName
+# Experimenting for private repository image
+
+# docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e "TRIVY_USERNAME=$USER_CREDENTIALS_USR" -e "TRIVY_PASSWORD=$USER_CREDENTIALS_PSW" aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light --input $imageName
+# docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e "TRIVY_USERNAME=$USER_CREDENTIALS_USR" -e "TRIVY_PASSWORD=$USER_CREDENTIALS_PSW" aquasec/trivy:0.17.2 -q image --exit-code 0 --severity CRITICAL --light --input $imageName
 
     # Trivy scan result processing
     exit_code=$?
